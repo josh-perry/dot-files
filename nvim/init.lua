e613a72a5f33d5d2f4ba8eed392d34272dae2af1
Josh Perry <joshperry@outlook.com>
2022-04-30 16:34:10 +0100

    Bind comma to :Ag
    For now, I need to find a better binding for this.

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,15 @@ vim.opt.number = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+
+local keys = {
+    ["n"] = {
+        [","] = ":Ag <CR>"
+    }
+}
+
+for keyType, t in pairs(keys) do
+    for k, v in pairs(t) do
+        vim.api.nvim_set_keymap(keyType, k, v, { noremap = false, silent = false })
+    end
+end
